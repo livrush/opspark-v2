7a5a3bc2a942e370f8f57e88362c7447fee7d3e4
Liv <w.phrush@gmail.com>
2019-10-29 15:30:34 -0500

    initial commit

---
+++ preact.config.js
@@ -0,0 +1,7 @@
+export default (config, env, helpers) => {
+  // let proxy = helpers.getLoadersByName(config, 'proxy-loader');
+  // const sass = proxy[1];
+  // const { includePaths } = sass.loader.options.options;
+  // sass.loader.options.sassOptions = { includePaths };
+  // delete sass.loader.options.options.includePaths;
+};
