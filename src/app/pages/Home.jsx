95746b104a5f26269296b1155910ffe27e649534
Liv <w.phrush@gmail.com>
2019-10-30 15:54:25 -0500

    add h2 style and adjust text container style

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -4,33 +4,45 @@ import Polygon from '../components/Polygon';
 const Home = () => (
   <div className="page container home">
     <Polygon backgroundImage="url('https://www.fillmurray.com/g/1000/700')">
-			<div className="container">
-				<div className="row">
-					<div className="col-8">
-						<h1 className="h1">
-							<span>WE CHANGE LIVES</span>
-							<span>WITH COMPUTER </span>
-							<span>PROGRAMMING SKILLS</span>
-						</h1>
-					</div>
-				</div>
-			</div>
+      <div className="row">
+        <div className="col-8">
+          <h1 className="h1">
+            WE CHANGE LIVES
+            <br />
+            WITH COMPUTER
+            <br />
+            PROGRAMMING SKILLS
+          </h1>
+        </div>
+      </div>
     </Polygon>
-    <div className="row text-section">
-      <h2 class="h2">What is Operation Spark?</h2>
-      <p className="col-12 paragraph">
-        Operation Spark, or OpSpark, is a New Orleans non-profit that teaches
-        the fundamentals of software development (also known as coding or
-        computer programming) to low-opportunity individuals by presenting the
-        fastest route to a career in software development. All students are
-        looking to begin a new career in the exciting field of software
-        development.
-      </p>
-      <p className="col-12 paragraph">
-        No past computer science experience is needed to get started in our
-        programs.
-      </p>
+    <div className="row text-container">
+      <div className="text-section">
+        <h2 class="h2">What is Operation Spark?</h2>
+        <p className="col-12 paragraph">
+          Operation Spark, or OpSpark, is a New Orleans non-profit that teaches
+          the fundamentals of software development (also known as coding or
+          computer programming) to low-opportunity individuals by presenting the
+          fastest route to a career in software development. All students are
+          looking to begin a new career in the exciting field of software
+          development.
+        </p>
+        <p className="col-12 paragraph">
+          No past computer science experience is needed to get started in our
+          programs.
+        </p>
+      </div>
+			<div className="text-section">
+	      <h2 class="h2">Hallebot's Quick Facts</h2>
+			</div>
     </div>
+    <Polygon alt backgroundImage="url('https://www.fillmurray.com/g/1000/700')">
+      <h1 className="h1">
+        WE OUR GRADS WORK
+        <br />
+        AT GREAT COMPANIES
+      </h1>
+    </Polygon>
   </div>
 );
 
