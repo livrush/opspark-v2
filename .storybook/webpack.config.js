3c872b7a2a9ebb85c0b3b6c4d7e6de80efb1e135
Liv <w.phrush@gmail.com>
2019-10-30 10:29:56 -0500

    remove unnecessary storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,16 +2,10 @@ const path = require('path');
 
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
-  // console.dir(config, { depth: null }) || config;
-  config.module.rules = config.module.rules.filter(rule => !rule.test.test(".scss"));
-
-  // Make whatever fine-grained changes you need
   config.module.rules.push({
     test: /\.scss$/,
-    use: ['node-sass', 'sass-loader'],
+    use: ['style-loader', 'css-loader', 'sass-loader'],
     include: path.resolve(__dirname, '../src/style'),
   });
-  // console.warn(config)
-  // Return the altered config
   return config;
 };
