6aa4a48cf6deec3ecfdcead4e11dfd90e0fb899d
Liv <w.phrush@gmail.com>
2019-10-31 10:01:03 -0500

    update cta halle background

--- src/app/components/CTA.jsx
+++ src/app/components/CTA.jsx
@@ -3,13 +3,11 @@ import { Link } from 'preact-router/match';
 
 const CTA = ({ actionText, buttonText, buttonLink }) => (
   <div className="call-to-action">
+    <div className="cta-halle-bg"></div>
     <div className="row">
-      <div className="col-8">
-
+      <div className="col-6 flex-col-center">
         <h3 className="h3">{actionText}</h3>
-        <div className="btn alt">
-          <Link>{buttonText}</Link>
-        </div>
+        <Link className="btn alt" href={buttonLink}>{buttonText}</Link>
       </div>
     </div>
   </div>
