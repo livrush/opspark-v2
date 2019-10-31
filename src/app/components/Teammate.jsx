14465e4833e57672e27deb894b39949f52311c79
Liv <w.phrush@gmail.com>
2019-10-31 11:34:28 -0500

    add section for selected teammate

--- src/app/components/Teammate.jsx
+++ src/app/components/Teammate.jsx
@@ -1,11 +1,12 @@
 import { h } from 'preact';
+import { Link } from 'preact-router/match';
 
 const Teammate = ({ info }) => (
-  <div className="teammate">
+  <Link href={`/team?mate=${info.name.replace(' ', '-').toLowerCase()}`} className="teammate">
     <img class="person teammate-headshot" src={`../../assets/headshots/${info.photo}`} alt={info.name} />
     <div className="teammate-name">{info.name}</div>
     <div className="teammate-title">{info.title}</div>
-  </div>
+  </Link>
 );
 
 export default Teammate;
--- src/app/pages/Team.jsx
+++ src/app/pages/Team.jsx
@@ -1,56 +1,87 @@
 import { h } from 'preact';
-import { Link } from 'preact-router/match';
 import Polygon from '../components/Polygon';
-import CTA from '../components/CTA';
 import Teammate from '../components/Teammate';
+import CTA from '../components/CTA';
 import { staff, board, advisors } from '../../copy/team';
 
-const Team = () => {
+const Team = ({ mate }) => {
+  let selectedTeammate = null;
+  if (mate) {
+    const team = [...staff, ...board, ...advisors];
+    for (var i = 0; i < team.length; i++) {
+      const currentTeammate = team[i];
+      const name = currentTeammate.name.replace(' ', '-').toLowerCase();
+      if (name === mate) {
+        console.log('Win!', name);
+        selectedTeammate = (
+          <div className="row flex-row-center teammate">
+            <div className="col-4">
+              <img
+                class="person teammate-headshot selected"
+                src={`../../assets/headshots/${currentTeammate.photo}`}
+                alt={currentTeammate.name}
+              />
+              <h1 className="h4">{currentTeammate.name}</h1>
+              <label for="job-title" className="h5">{currentTeammate.title}</label>
+              <div className="teammate-change-container flex-row">
+                <button className="text alt">&lt;</button>
+                <img src="../assets/lightning.svg" alt="lightning bolt"/>
+                <button className="text alt">&gt;</button>
+              </div>
+            </div>
+            <div className="col-6 left">
+              {currentTeammate.paragraphs.map(para => <p>{para}</p>)}
+            </div>
+          </div>
+        )
+        break;
+      }
+    }
+  }
+
   return (
     <div className="page container home">
       <Polygon backgroundImage="url('https://www.fillmurray.com/g/1000/700')">
-        <h1 className="h1">
-          OUR MISSION
-        </h1>
-        <p>
-          Operation Spark is opening a new door to prosperity
-          for low-opportunity individuals by presenting
-          the fastest route to a career in software development.
-        </p>
+        <div className="col-8 flex-col-center">
+          <h1 className="h1">OUR MISSION</h1>
+          <p class="h5">
+            Operation Spark is opening a new door to prosperity for
+            low-opportunity individuals by presenting the fastest route to a
+            career in software development.
+          </p>
+        </div>
       </Polygon>
       <div className="row text-container">
+        <div className="selected-teammate">{selectedTeammate}</div>
         <h2 className="h2">Staff</h2>
         <div className="flex-row-wrap">
-        {
-          staff.map(teammate => (
+          {staff.map(teammate => (
             <div className="col-3">
               <Teammate info={teammate} />
             </div>
-          ))
-        }
+          ))}
         </div>
         <h2 className="h2">Board of Directors</h2>
         <div className="flex-row-wrap">
-        {
-          board.map(teammate => (
+          {board.map(teammate => (
             <div className="col-3">
               <Teammate info={teammate} />
             </div>
-          ))
-        }
+          ))}
         </div>
         <h2 className="h2">Advisory Board</h2>
         <div className="flex-row-wrap">
-        {
-          advisors.map(teammate => (
+          {advisors.map(teammate => (
             <div className="col-3">
               <Teammate info={teammate} />
             </div>
-          ))
-        }
+          ))}
         </div>
       </div>
-      <CTA actionText="Start your Software Journey Today" buttonText="Join us" />
+      <CTA
+        actionText="Start your Software Journey Today"
+        buttonText="Join us"
+      />
     </div>
   );
 }
