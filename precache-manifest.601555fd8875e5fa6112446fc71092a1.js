03acf2ffb272c248beb5a523d44681d6327742cc
Liv <w.phrush@gmail.com>
2019-11-01 15:54:06 -0500

    Updates

---
+++ precache-manifest.601555fd8875e5fa6112446fc71092a1.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b69fdc22598ac051d52c10c9212f54",
+    "url": "/assets/headshots/akintunde-pounds.jpg"
+  },
+  {
+    "revision": "619000e9766ca05bf99827ce626d2e74",
+    "url": "/assets/headshots/allen-square.jpg"
+  },
+  {
+    "revision": "420b2d8ef25ccaa6d2e211444b4768ee",
+    "url": "/assets/headshots/andy-nguyen.jpg"
+  },
+  {
+    "revision": "9ff2e2ab17481e41ff4c98dbfed1638b",
+    "url": "/assets/headshots/ashton-martin.jpg"
+  },
+  {
+    "revision": "2529cda6bcfbeff50c1065c18fa26747",
+    "url": "/assets/headshots/ben-spector.jpg"
+  },
+  {
+    "revision": "f12e5d17a8f6a0b80284e4da432b10f9",
+    "url": "/assets/headshots/cain-watson.jpg"
+  },
+  {
+    "revision": "6d7fd7a9dd036ce754fec86d4b2079a1",
+    "url": "/assets/headshots/chike-aguh.jpg"
+  },
+  {
+    "revision": "ac1b6236220461e254857c3c39539ad1",
+    "url": "/assets/headshots/christine-saucier.jpg"
+  },
+  {
+    "revision": "91ba8d33de9eb30f9b940f4d7380d483",
+    "url": "/assets/headshots/cortez-washington.jpg"
+  },
+  {
+    "revision": "6ceed55aff7a9c13c5f814c883934d69",
+    "url": "/assets/headshots/daniel-kogler.jpg"
+  },
+  {
+    "revision": "0837df1817f631fbde6f0840cca8bf7e",
+    "url": "/assets/headshots/don-stridacchio.jpg"
+  },
+  {
+    "revision": "e73c6ab595dbe06bc8787991160396ad",
+    "url": "/assets/headshots/grady-fitzpatrick.jpg"
+  },
+  {
+    "revision": "0483437d772a185872c62766b323c64a",
+    "url": "/assets/headshots/harvey-sanders.jpg"
+  },
+  {
+    "revision": "85029e279eb82df30ef8d85c01133333",
+    "url": "/assets/headshots/jeffrey-serpas.jpg"
+  },
+  {
+    "revision": "5eb313b9ecf50e218ad4833d6d23075b",
+    "url": "/assets/headshots/john-fraboni.jpg"
+  },
+  {
+    "revision": "b8b3912cd3aad8ce5dc652eab91c0630",
+    "url": "/assets/headshots/justin-connor.jpg"
+  },
+  {
+    "revision": "b5634f94b51196309564ef809dfdd9b3",
+    "url": "/assets/headshots/kaelyn-chresfield.jpg"
+  },
+  {
+    "revision": "aea93411afe2f06e249066c28ebf417c",
+    "url": "/assets/headshots/kendall-spears.jpg"
+  },
+  {
+    "revision": "d962e0c3f983b79adbad7f233d93c0f8",
+    "url": "/assets/headshots/kerry-hegarty.jpg"
+  },
+  {
+    "revision": "99fd356e4445c43e01a008b152303e14",
+    "url": "/assets/headshots/kim-farrell.jpg"
+  },
+  {
+    "revision": "155bb6058077623d3a442dce3038e8c4",
+    "url": "/assets/headshots/laura-parker.jpg"
+  },
+  {
+    "revision": "19ab376513b001242e292d7dc30e2f41",
+    "url": "/assets/headshots/mayukh-raychaudhuri.jpg"
+  },
+  {
+    "revision": "ac313418673932ef34b7e74aaf29f893",
+    "url": "/assets/headshots/myron-l-mitchell.jpg"
+  },
+  {
+    "revision": "10e994263199387fee92bcf12a3852e2",
+    "url": "/assets/headshots/olivia-sabo-rush.jpg"
+  },
+  {
+    "revision": "672de37d207e321cfd522ce1aab20aa4",
+    "url": "/assets/headshots/ramsey-green.jpg"
+  },
+  {
+    "revision": "6783e4e515760a44f00926c827da3aa8",
+    "url": "/assets/headshots/ryan-mcfarland.jpg"
+  },
+  {
+    "revision": "3b41ce48e24c18fbafc1fe9c522d73b9",
+    "url": "/assets/headshots/stacey-sharer.jpg"
+  },
+  {
+    "revision": "d70fa9c559464d6ff90a90e263369391",
+    "url": "/assets/headshots/taylor-shephard.jpg"
+  },
+  {
+    "revision": "fac2ed592996fd0ddb8fecb461cfcc36",
+    "url": "/assets/headshots/vera-triplett.jpg"
+  },
+  {
+    "revision": "d017f738e6fb9bb5338eaa00b5d46189",
+    "url": "/assets/headshots/virginia-tickles.jpg"
+  },
+  {
+    "revision": "27feff5c85366f34d161d45b0193e94d",
+    "url": "/assets/headshots/william-bishop.jpg"
+  },
+  {
+    "revision": "1ff0219217e1c24d42147bf0b05c9b9c",
+    "url": "/assets/headshots/zuri-gracin.jpg"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "7dbf1948a471679a7af16faa5f59a2c2",
+    "url": "/assets/logos/4wwltv.png"
+  },
+  {
+    "revision": "9210dad8641a08db75ba189b384d6dec",
+    "url": "/assets/logos/WKKF.png"
+  },
+  {
+    "revision": "61aa36c7664cb33edb2fc46583d723ee",
+    "url": "/assets/logos/advocate.png"
+  },
+  {
+    "revision": "71785c1da8d87d7a52e9b4e486521d10",
+    "url": "/assets/logos/att.png"
+  },
+  {
+    "revision": "5a1db78a11ba443228c832106a0bdd1d",
+    "url": "/assets/logos/capital-one.png"
+  },
+  {
+    "revision": "d88f4b3ba0554cbe20d287a7d1033281",
+    "url": "/assets/logos/entergy.png"
+  },
+  {
+    "revision": "badd3e9c4b4b440971a242af467eabab",
+    "url": "/assets/logos/forbes.png"
+  },
+  {
+    "revision": "971d2bcae8c7e4d8086df02d45d78998",
+    "url": "/assets/logos/google.png"
+  },
+  {
+    "revision": "9aaddcf7ba32d906bbd3d0ac6de2894e",
+    "url": "/assets/logos/white-house.png"
+  },
+  {
+    "revision": "0c3ec29a1db21207d891",
+    "url": "/bundle.0c3ec.js"
+  },
+  {
+    "revision": "0c3ec29a1db21207d891",
+    "url": "/bundle.a28ec.css"
+  },
+  {
+    "revision": "eb8e8fc760ec3f0265d3bd3c98afa224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e49433b3f223e35c6dd3",
+    "url": "/polyfills.e4943.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "035fe5842ec8eb147eb2909c17664517",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "beec18519751ae79bfb6c12cb9a3246f",
+    "url": "/sw.js"
+  }
+]);
