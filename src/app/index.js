676955322cc57fbceb863846f3ff971fda3cd68c
Liv <w.phrush@gmail.com>
2019-11-01 14:29:19 -0500

    add scroll to top of page when page changes

--- src/app/index.js
+++ src/app/index.js
@@ -16,6 +16,7 @@ export default class App extends Component {
 	 *	@param {string} event.url	The newly routed URL
 	 */
 	handleRoute = e => {
+		window.scroll(0, 0);
 		this.currentUrl = e.url;
 	};
 
