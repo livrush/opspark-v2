0e4d9ce1b785f14369a28460ca939faae4176a82
Liv <w.phrush@gmail.com>
2019-11-01 12:24:54 -0500

    add sections to homepage

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -36,10 +36,32 @@ const Home = () => (
           looking to begin a new career in the exciting field of software
           development.
         </p>
-        <p className="col-12 paragraph">
+        <p className="col-12 paragraph bold">
           No past computer science experience is needed to get started in our
           programs.
         </p>
+        <div className="row">
+          <div className="col-3">
+            <a href="" target="_blank" >
+              <img class="news-logo" src="../../assets/logos/forbes.png" alt="" />
+            </a>
+          </div>
+          <div className="col-3">
+            <a href="" target="_blank" >
+              <img class="news-logo" src="../../assets/logos/white-house.png" alt="" />
+            </a>
+          </div>
+          <div className="col-3">
+            <a href="https://www.theadvocate.com/baton_rouge/news/politics/article_4309e250-4e61-51e8-974c-a50c5f0b92d3.html" target="_blank" >
+              <img class="news-logo" src="../../assets/logos/4wwltv.png" alt="" />
+            </a>
+          </div>
+          <div className="col-3">
+            <a href="" target="_blank" >
+              <img class="news-logo" src="../../assets/logos/advocate.png" alt="" />
+            </a>
+          </div>
+        </div>
       </div>
       <div className="text-section">
         <h2 class="h2">Hallebot's Quick Facts</h2>
@@ -60,35 +82,21 @@ const Home = () => (
     </Polygon>
     <div className="text-container">
       <div className="row center">
-        <div className="col-4 flex-col-center">
-          <div
-            src=""
-            alt=""
-            class="person"
-            style="height: 5rem; width: 5rem"
-          ></div>
-          <img src="" alt="" />
+        <div className="col-4 flex-col-center icon-bullet">
+          <img src="../../assets/home-money.svg" alt="money" />
           <p className="paragraph">Provide significant lifetime earnings</p>
         </div>
-        <div className="col-4 flex-col-center">
-          <div
-            src=""
-            alt=""
-            class="person"
-            style="height: 5rem; width: 5rem"
-          ></div>
-          <img src="" alt="" />
+        <div className="col-4 flex-col-center icon-bullet">
+          <img
+            src="../../assets/home-college.svg"
+            alt="crossed out graduation hat"
+          />
           <p className="paragraph">
             Don't require college degrees/certifications
           </p>
         </div>
-        <div className="col-4 flex-col-center">
-          <div
-            src=""
-            alt=""
-            class="person"
-            style="height: 5rem; width: 5rem"
-          ></div>
+        <div className="col-4 flex-col-center icon-bullet">
+          <img src="../../assets/home-globe.svg" alt="globe" />
           <p className="paragraph">
             Available in every industry across the world
           </p>
